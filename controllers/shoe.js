fe03e2eb34fe93fac6c564d49570a7cb395f90ef
S550608 <S550608@NWMISSOURI.EDU>
2022-11-09 18:08:14 -0600

    Created and Modified the files

--- controllers/shoe.js
+++ controllers/shoe.js
@@ -15,91 +15,48 @@ exports.shoe_create_post = async function(req, res) {
     document.Shoe_Company = req.body.Shoe_Company;
     document.Shoe_Size = req.body.Shoe_Size;
     document.Shoe_Rating = req.body.Shoe_Rating;
-
     try{
-
         let result = await document.save();
-
         res.send(result);
-
     }
-
     catch(err){
-
         res.status(500);
-
         res.send(`{"error": ${err}}`);
-
     }  
-
 };
-
-
-
- 
 
 // Handle shoe delete form on DELETE.
-
 exports.shoe_delete = function(req, res) {
-
     res.send('NOT IMPLEMENTED: shoe delete DELETE ' + req.params.id);
-
 };
-
- 
 
 // Handle shoe update form on PUT.
 
 exports.shoe_update_put = function(req, res) {
-
     res.send('NOT IMPLEMENTED: shoe update PUT' + req.params.id);
-
 };
-
-
 
 exports.shoe_list = async function(req, res) {
-
     try{
-
         theshoes = await shoe.find();
-
         res.send(theshoes);
-
     }
-
     catch(err){
-
         res.status(500);
-
         res.send(`{"error": ${err}}`);
-
     }  
-
 };
 
-
-
 // VIEWS
-
 // Handle a show all view
 
 exports.shoe_view_all_Page = async function(req, res) {
-
     try{
-
         theshoes = await shoe.find();
-
         res.render('shoe', { title: 'shoe Search Results', results: theshoes });
-
     }
-
     catch(err){
-
         res.status(500);
-
         res.send(`{"error": ${err}}`);
-
     }  
-
 };
