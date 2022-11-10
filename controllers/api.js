fdd806819a6f23cca40c645525ab41d8a183bc50
S550608 <S550608@NWMISSOURI.EDU>
2022-11-09 18:03:48 -0600

    Created and Modified the files

--- controllers/api.js
+++ controllers/api.js
@@ -1,8 +1,9 @@
-exports.api = function (req, res) {
+// API for our resources
+exports.api = function(req, res) {
     res.write('[');
-    res.write('{"resource":"films", ');
+    res.write('{"resource":"shoe", ');
     res.write(' "verbs":["GET","PUT", "DELETE"] ');
     res.write('}');
     res.write(']')
     res.send();
-};
+    };
--- controllers/shoe.js
+++ controllers/shoe.js
@@ -1,54 +1,105 @@
 var shoe = require('../models/shoe');
 // List of all shoes
-exports.shoe_list = async function (req, res) {
-    try {
-        theshoes = await shoe.find();
-        res.send(theshoes);
-    }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }
+exports.shoe_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: shoe list');
 };
 // for a specific shoe.
-exports.shoe_detail = function (req, res) {
+exports.shoe_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: shoe detail: ' + req.params.id);
 };
 // Handle shoe create on POST.
-exports.shoe_create_post = async function (req, res) {
+exports.shoe_create_post = async function(req, res) {
     console.log(req.body)
     let document = new shoe();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"shoe_type":"goat", "cost":12, "size":"large"}
-    document.name = req.body.name;
-    document.duration = req.body.duration;
-    document.director = req.body.director;
-    try {
+    document.Shoe_Name = req.body.Shoe_Name;
+    document.Shoe_Company = req.body.Shoe_Company;
+    document.Shoe_Size = req.body.Shoe_Size;
+    document.Shoe_Rating = req.body.Shoe_Rating;
+
+    try{
+
         let result = await document.save();
+
         res.send(result);
+
     }
-    catch (err) {
+
+    catch(err){
+
         res.status(500);
+
         res.send(`{"error": ${err}}`);
-    }
+
+    }  
+
 };
+
+
+
+ 
+
 // Handle shoe delete form on DELETE.
-exports.shoe_delete = function (req, res) {
+
+exports.shoe_delete = function(req, res) {
+
     res.send('NOT IMPLEMENTED: shoe delete DELETE ' + req.params.id);
+
 };
+
+ 
+
 // Handle shoe update form on PUT.
-exports.shoe_update_put = function (req, res) {
+
+exports.shoe_update_put = function(req, res) {
+
     res.send('NOT IMPLEMENTED: shoe update PUT' + req.params.id);
+
 };
-exports.flim_view_all_Page = async function (req, res) {
-    try {
+
+
+
+exports.shoe_list = async function(req, res) {
+
+    try{
+
         theshoes = await shoe.find();
-        res.render('shoes', { title: 'shoe Search Results', results: theshoes });
+
+        res.send(theshoes);
+
     }
-    catch (err) {
+
+    catch(err){
+
         res.status(500);
+
         res.send(`{"error": ${err}}`);
+
+    }  
+
+};
+
+
+
+// VIEWS
+
+// Handle a show all view
+
+exports.shoe_view_all_Page = async function(req, res) {
+
+    try{
+
+        theshoes = await shoe.find();
+
+        res.render('shoe', { title: 'shoe Search Results', results: theshoes });
+
     }
+
+    catch(err){
+
+        res.status(500);
+
+        res.send(`{"error": ${err}}`);
+
+    }  
+
 };
